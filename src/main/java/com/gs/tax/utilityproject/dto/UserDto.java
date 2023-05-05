4d93ff485d58383efff3239f6db0c54bdbae759e
anshultongia <atongia10@gmail.com>
2023-05-05 16:08:52 -0400

    First Checkin

--- src/main/java/com/gs/tax/utilityproject/dto/UserDto.java
+++ src/main/java/com/gs/tax/utilityproject/dto/UserDto.java
@@ -16,6 +16,13 @@ public class UserDto {
     @NotEmpty(message = "Email must not be empty")
     private String email;
 
+    public UserDto(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
     public Long getId() {
         return id;
     }
---
+++ src/test/java/com/gs/tax/utilityproject/UserControllerTest.java
@@ -0,0 +1,56 @@
+package com.gs.tax.utilityproject;
+
+import com.gs.tax.utilityproject.controller.UserController;
+import com.gs.tax.utilityproject.dto.UserDto;
+import com.gs.tax.utilityproject.service.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserServiceImpl userServiceImpl;
+
+    private List<UserDto> users;
+
+    @BeforeEach
+    void setUp() {
+        users = List.of(
+                new UserDto(1L, "John", "Doe", "a@gs.com"),
+                new UserDto(2L, "Jane", "Doe", "b@gs.com")
+        );
+    }
+
+    @Test
+    void testGetAllUsers() throws Exception {
+        when(userServiceImpl.findAll()).thenReturn(users);
+
+        mockMvc.perform(get("/api/v1/users"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(users.size()))
+                .andExpect(jsonPath("$[0].id").value(users.get(0).getId()))
+                .andExpect(jsonPath("$[0].firstName").value(users.get(0).getFirstName()))
+                .andExpect(jsonPath("$[0].lastName").value(users.get(0).getLastName()))
+                .andExpect(jsonPath("$[1].id").value(users.get(1).getId()))
+                .andExpect(jsonPath("$[1].firstName").value(users.get(1).getFirstName()))
+                .andExpect(jsonPath("$[1].lastName").value(users.get(1).getLastName()));
+    }
+}
