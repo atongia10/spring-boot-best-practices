651e19b24dbc05a908314fd3c894be77ec5b53ff
anshultongia <atongia10@gmail.com>
2023-05-05 13:27:32 -0400

    First Checkin

---
+++ src/main/java/com/gs/tax/utilityproject/UtilityProjectApplication.java
@@ -0,0 +1,11 @@
+package com.gs.tax.utilityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gs/tax/utilityproject/UtilityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gs.tax.utilityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
